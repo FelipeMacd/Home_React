3af2bd89ac8d7cef8033551d2d05fb135ab12564
FelipeMcdo <felipemcdo7@gmail.com>
2024-08-06 13:49:49 -0300

    testando o useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
 import Home from './pages/home/Home';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [valor, setValor] = useState(0);
+
+  function handleClick(){
+    setValor(valor + 1);
+  }
+
   return (
-    <Home title="Componente Home"
-    description="Este é um componente Home que recebe props"/>
+    //  <Home  title="Componente Home"
+    //  description="Este é um componente Home que recebe props."/>
+    <div>
+      <p>O valo é: {valor}</p>
+      <button onClick={handleClick}>Adicionar</button>
+    </div>
   );
 }
 
