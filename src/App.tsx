39027733ad6414a73a7d5a80bde0e8e44101deae
FelipeMcdo <felipemcdo7@gmail.com>
2024-08-06 14:15:30 -0300

    testando useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import Home from './pages/home/Home';
 import './App.css';
-import { useState } from 'react';
+import React from 'react';
 
 function App() {
-
-  const [valor, setValor] = useState(0);
-
-  function handleClick(){
-    setValor(valor + 1);
-  }
-
+ 
   return (
-    //  <Home  title="Componente Home"
-    //  description="Este é um componente Home que recebe props."/>
-    <div>
-      <p>O valo é: {valor}</p>
-      <button onClick={handleClick}>Adicionar</button>
-    </div>
+    <>
+      <Home/>
+    </>
   );
 }
 
